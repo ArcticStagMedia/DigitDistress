0def717b4a82ad11ecbacb6fb52af2b1cde0ca53
unknown <mallanson@SYD-803-1252-W.syd.sae>
2015-03-17 09:54:36 +1100

    Pushing Clipboard

---
+++ Assets/Scripts/TestButtonScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButtonScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnMouseOver()
+	{
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+
+	}
+
+	void OnMouseDown()
+	{
+		renderer.material.color = Color.green;
+	}
+
+}
