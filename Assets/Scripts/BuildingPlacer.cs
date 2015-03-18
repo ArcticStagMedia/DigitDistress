4924aacaa34e7c93f977fb972ecc6f2ab71fd697
Callum Bullard <stagdage@gmail.com>
2015-03-18 15:18:58 +1100

    Addition of building tools

---
+++ Assets/Scripts/BuildingPlacer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BuildingPlacer : MonoBehaviour {
+
+    private string txtClickU = "Click \"U\" to view Building Interface";
+    public Text mainCameraCanvas;
+    public GameObject gameCanvas;
+    public Camera mainCamera;
+    private bool ableToAccessUI = false;
+    
+    // Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (ableToAccessUI && Input.GetKeyDown(KeyCode.U))
+        {
+                gameCanvas.SetActive(true);
+                mainCameraCanvas.text = null;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            mainCameraCanvas.text=txtClickU;
+            ableToAccessUI = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {     
+            mainCameraCanvas.text = null;
+            ableToAccessUI = false;
+    }
+}
---
+++ Assets/Scripts/PlacementOrb.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacementOrb : MonoBehaviour {
+
+    public GameObject BuilderObject;
+
+    
+	// Use this for initialization
+	void Start () 
+    {
+        BuilderObject.SetActive(false);
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            BuilderObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+            BuilderObject.SetActive(false);
+    }
+}
