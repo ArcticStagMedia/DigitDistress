d68c3063883ec59f5f56946050c71733f08fe226
unknown <jramirez26@SYD-803-1270-W.syd.sae>
2015-03-18 15:30:34 +1100

    Changed building plots, icon, and roads

---
+++ Assets/TransportScripts/BUSmovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BUSmovement : MonoBehaviour {
+
+	public BoardBus BBmovement;
+	public BoardBus BBmovement1;
+
+	public Transform[] Waypoints;
+	public float moveSpeed;
+	private int currentPoint;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = Waypoints [0].position;
+		currentPoint = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt (Waypoints [currentPoint]);
+
+
+		if (transform.position == Waypoints [currentPoint].position) 
+		{
+
+			currentPoint++;
+		}
+
+		if (currentPoint >= Waypoints.Length) 
+		{
+			currentPoint = 0;
+		}
+		if(BBmovement.CountAI == 6)
+		{
+			transform.position = Vector3.MoveTowards (transform.position, Waypoints [currentPoint].position, moveSpeed * Time.deltaTime);
+		}
+		if(BBmovement1.CountAI == 6)
+		{
+			transform.position = Vector3.MoveTowards (transform.position, Waypoints [currentPoint].position, moveSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/TransportScripts/BusStopTrigger.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BusStopTrigger : MonoBehaviour {
+	public BoardBus BB;
+	public bool  BusIsAtStop = false;
+	public Transform BusStop;
+	//GameObject Digits;
+	// Use this for initialization
+	void Start () {
+		//Digits = GameObject.Find("Digits");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+	public void OnTriggerStay(Collider other)
+	{ 
+		
+		if (other.gameObject.tag == "Bus") 
+		{
+			BusIsAtStop = true;
+			Debug.Log ("Bus Stopped");
+			//BB.CountAI = 0;
+		} 
+		
+		if (other.gameObject.tag == "Digit" && BusIsAtStop == true) 
+		{
+			//BB.CountAI++;
+			
+			Debug.Log ("Leaving Stop");
+			other.gameObject.transform.position = BusStop.position;
+			other.gameObject.renderer.enabled = (true);
+			
+			//if (BB.CountAI <= 6) {	
+
+				
+			//} 
+			
+		}
+		if (other.gameObject.tag == "Digit") 
+		{
+			Debug.Log("There in trigger");
+		}
+		
+		
+	}
+//	public void OnTriggerStay(Collider other)
+//	{		if (other.gameObject.tag == "Bus") {
+//			BusIsAtStop = true;
+//			BB.CountAI = 0;
+//
+//		} 
+//
+//
+//	}
+
+}
