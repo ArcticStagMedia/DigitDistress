59642bc599b22b8d433c7927836d214ee4579d2b
Callum Bullard <stagdage@gmail.com>
2015-03-19 21:22:58 +1100

    -Addition of remove building script
    -modification of Building Placement Plot Parent Prefab

---
+++ Assets/Scripts/RemoveBuilding.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveBuilding : MonoBehaviour {
+
+    private GameObject currentBuilding;
+    public GameObject buildingPlaceholder;
+
+    // Use this for initialization
+	void Start () 
+    {
+        currentBuilding = transform.Find("B_CoffeeShop").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    public void RemoveTheBuilding()
+    {
+        currentBuilding.SetActive(false);
+        buildingPlaceholder.SetActive(true);
+    }
+}
