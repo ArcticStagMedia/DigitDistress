da7bd02cc57532d661df65b907247e16023b65f1
Callum Bullard <stagdage@gmail.com>
2015-03-19 19:50:24 +1100

    Recreating building Plot so that on creation of building you don't lose rest of prefab

---
+++ Assets/Scripts/PlaceBuilding.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceBuilding : MonoBehaviour {
+
+    private GameObject buildArea;
+    public GameObject building;
+
+	// Use this for initialization
+	void Start () {
+        building.SetActive(false);
+        buildArea = transform.Find("BuildingPlacementPlotChild").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void BuildBuilding ()
+    {
+        buildArea.SetActive(false);
+        building.SetActive(true);
+    }
+}
