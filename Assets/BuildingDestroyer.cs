996d7b1f703eb2007b055d5b270fdd3a4fb65c15
TavisMolyneux <tavismolyneux@gmail.com>
2015-03-24 16:17:26 +1100

    New BuildingPlacement
    Added a working building placement that can delete old buildings and
    place new ones, also has a UI with multiple panels.

---
+++ Assets/BuildingDestroyer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingDestroyer : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.N)) 
+		{
+			print ("test");
+			Destroy();
+		}
+	
+	}
+
+	public void Destroy()
+	{
+		Destroy (this.gameObject);
+	}
+}
