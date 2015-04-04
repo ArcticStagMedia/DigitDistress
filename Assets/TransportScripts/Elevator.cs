6d4982fd40f6b932a5cf3afea01448d1dcfeebe7
Mackenzie Allanson <111294@student.sae.edu.au>
2015-04-04 13:44:28 +1100

    Start of transport into greybox

---
+++ Assets/TransportScripts/Elevator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+
+	public Transform[] Waypoints;
+	public float moveSpeed;
+	private int currentPoint;
+	public float EleTime;
+	public float ElevatorWait;
+	public float ElevatorRiseTime;
+	// Use this for initialization
+	void Start () {
+		transform.position = Waypoints [0].position;
+		currentPoint = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		EleTime += Time.deltaTime;
+
+		if (EleTime >= ElevatorRiseTime) 
+		{
+			EleTime = 0;
+		}
+
+		if (transform.position == Waypoints [currentPoint].position) 
+		{
+			currentPoint++;
+		}
+		
+		if (currentPoint >= Waypoints.Length) 
+		{
+			currentPoint = 0;
+		}
+		if (EleTime >= ElevatorWait) 
+		{
+			transform.position = Vector3.MoveTowards (transform.position, Waypoints [currentPoint].position, moveSpeed * Time.deltaTime);
+		}
+	}
+}
