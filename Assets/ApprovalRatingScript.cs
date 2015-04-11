57c50e05cda871ae9b369c777bbb975896cdbf8c
TavisMolyneux <tavismolyneux@gmail.com>
2015-04-11 22:32:51 +1000

    finance Section made
    Listsssss

---
+++ Assets/ApprovalRatingScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class ApprovalRatingScript : MonoBehaviour 
+{
+	public Image ApprovalRating;
+	public float FillAmount;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		ApprovalRating = GetComponent<Image> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ApprovalRating.fillAmount = .5f;
+
+	
+	}
+}
---
+++ Assets/MainNanoClipTextScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class MainNanoClipTextScript : MonoBehaviour 
+{
+	public Text TotalText;
+	public int ByteTotal;
+
+	// Use this for initialization
+	void Start () 
+	{
+		TotalText = GetComponent<Text> ();
+		TotalText.text = "Name: Mayor Digit" + "\n" + "System: Nanoclip OS" + "\n" + "Bytes: " + ByteTotal;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
