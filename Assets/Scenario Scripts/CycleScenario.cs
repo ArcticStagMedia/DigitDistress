6f54ef4edf61d42c0c9fc2768be111368dc6a831
Mackenzie Allanson <111294@student.sae.edu.au>
2015-04-28 23:44:17 +1000

    Added Cycles in that increment after 2 mins

---
+++ Assets/Scenario Scripts/CycleScenario.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CycleScenario : MonoBehaviour {
+	 
+	public float CycleTimer;
+	private int CycleNumber;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CycleTimer += Time.deltaTime;
+
+		if (CycleTimer >= 120) 
+		{
+			CycleNumber++;
+			CycleTimer = 0;
+		}
+
+		Debug.Log ("Cycle: " + CycleNumber);
+	}
+}
